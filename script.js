b8459f5c028dc3e6e98b94abd4dbfb061a59d91c
JaredS04 <105817822+JaredS04@users.noreply.github.com.>
2024-11-25 17:12:19 -0700

    Added some code to JS & CSS

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 /* Create the divs using JavaScript. 
 Don’t try to create them by hand by copying and pasting them in your HTML file!
 */
-var div = document.createElement("div");
-div.style.width = "16px";
-div.style.height = "16px";
-div.style.background = "grey";
-div.style.color = "black";
-div.innerHTML = "EtchASketch";
 
-document.getElementsById("main").appendChild(div);
+// Creates square of divs
+const container = document.getElementById("container");
+
+function makeDivs(numDivs){
+    for (let d = 0; d <numDivs; d++){
+        let cells = document.createElement('div');
+        container.appendChild(cells)
+    }
+}
+
+makeDivs(256)
